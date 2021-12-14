208e8d1e9d5a573404e92c9d9810bd95700c6805
Sergii Svystun <sergiisvystun@gmail.com>
2021-12-14 17:14:05 +0200

    taxi-service final version

---
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -0,0 +1,27 @@
+package taxi.service;
+
+import java.util.Optional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import taxi.exception.AuthenticationException;
+import taxi.lib.Inject;
+import taxi.lib.Service;
+import taxi.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+    @Inject
+    private DriverService driverService;
+
+    @Override
+    public Driver login(String login, String password) throws AuthenticationException {
+        Optional<Driver> driver = driverService.findByLogin(login);
+        if (driver.isPresent() && driver.get().getPassword().equals(password)) {
+            logger.info("Driver was logged in successfully. Params: Driver login = {}", login);
+            return driver.get();
+        }
+        logger.error("Login is unsuccessful. Params: Driver login = {}", login);
+        throw new AuthenticationException("Driver login or password was incorrect");
+    }
+}
